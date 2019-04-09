5eee63314576ee2496a5d36cdfda5cf395c58d94
Leandre Blanchard <leandre.blanchard@parrot.com>
2019-04-09 14:30:51 +0200

    [fix] compilation warning

--- src/lsize.c
+++ src/lsize.c
@@ -10,7 +10,7 @@
 inline size_t lbuffer_lsize(lbuffer_t *buffer)
 {
   if (buffer->reader < buffer->writer)
-    return (buffer->size - buffer->writer + buffer->reader);
+    return (buffer->size - (size_t)buffer->writer + (size_t)buffer->reader);
   else if (buffer->reader > buffer->writer)
     return (buffer->reader - buffer->writer);
   return ((buffer->empty) ? buffer->size : 0);
